6df5a6e799fe097001ad7803ea11c3ff597816ef
satyamhacker <satyamgrandmaster@gmail.com>
2025-08-10 20:26:59 +0530

    refactor(pages): reorganize user panel pages into a dedicated subpackage
    - Move all user panel-related pages into a new `user_panel_pages` subpackage
    - Update import paths in test files to reflect the new directory structure
    - This change prepares for the addition of admin panel pages, separating them from user panel pages

--- tests/user_panel_tests/test_buy_now_page.py
+++ tests/user_panel_tests/test_buy_now_page.py
@@ -5,8 +5,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
 from dotenv import load_dotenv
 from utils.driver_setup import get_driver
-from pages.login_page import LoginPage
-from pages.buy_now_page import BuyNowPage
+from pages.user_panel_pages.login_page import LoginPage
+from pages.user_panel_pages.buy_now_page import BuyNowPage
 import time
 
 load_dotenv()
--- tests/user_panel_tests/test_main_zone.py
+++ tests/user_panel_tests/test_main_zone.py
@@ -5,8 +5,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
 from dotenv import load_dotenv
 from utils.driver_setup import get_driver
-from pages.login_page import LoginPage
-from pages.main_zone_page import MainZonePage
+from pages.user_panel_pages.login_page import LoginPage
+from pages.user_panel_pages.main_zone_page import MainZonePage
 import time
 
 load_dotenv()
